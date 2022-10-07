6cea00e273dcd32b6e0e6ee11855a8e2905e9066
FazaZas <fazanur17@gmail.com>
2022-10-07 14:01:50 +0700

    setup admin area

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,19 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ Route::get('/', [FrontendController::class, 'index'])->name('index');
+ Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('details');
+ Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+ Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+//routing group
+Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index'); // cara akses routing dashboard.index
+    Route::middleware(['admin'])->group(function () {
+        //
+    });
 });
